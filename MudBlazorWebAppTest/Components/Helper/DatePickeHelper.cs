fe0f70b2f4b2b6cb664aeb393ee4b67a3ed8cdfe
Amin <KahramAmin@gmail.com>
2024-02-29 16:38:42 +0330

    MudDatePicker fixed to change culture dynamically

--- MudBlazorWebAppTest/Components/Helper/DatePickeHelper.cs
+++ MudBlazorWebAppTest/Components/Helper/DatePickeHelper.cs
@@ -1,7 +1,7 @@
 ﻿using System.Globalization;
 using System.Reflection;
 
-public static class PersianDateExtensionMethods
+public static class DateExtensionMethods
 {
     private static CultureInfo _culture;
 
@@ -40,6 +40,34 @@ public static class PersianDateExtensionMethods
             _culture.NumberFormat.NumberDecimalSeparator = "/";
             _culture.NumberFormat.DigitSubstitution = DigitShapes.NativeNational;
             _culture.NumberFormat.NumberNegativePattern = 0;
+        }
+        return _culture;
+    }
+    public static CultureInfo GetGregorianCulture()
+    {
+        const string cultureName = "en-US";
+
+        if (_culture == null)
+        {
+            _culture = new CultureInfo(cultureName);
+
+            DateTimeFormatInfo formatInfo = _culture.DateTimeFormat;
+
+            formatInfo.AbbreviatedDayNames = new[] { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+            formatInfo.DayNames = new[] { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+            formatInfo.MonthNames = new[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "" };
+
+            _culture.DateTimeFormat = new CultureInfo("en-US").DateTimeFormat;
+
+            Calendar gregorianCalendar = new GregorianCalendar();
+
+            FieldInfo fieldInfo = _culture.GetType().GetField("calendar", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (fieldInfo != null)
+                fieldInfo.SetValue(_culture, gregorianCalendar);
+
+            FieldInfo info = formatInfo.GetType().GetField("calendar", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (info != null)
+                info.SetValue(formatInfo, gregorianCalendar);
         }
         return _culture;
     }
